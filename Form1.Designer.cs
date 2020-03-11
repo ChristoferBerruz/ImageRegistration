3eec54e6564f175ad3c6f60aac059394b0f26a40
ChristoferBerruz <46355467+ChristoferBerruz@users.noreply.github.com>
2020-03-10 21:57:22 -0400

    Required to run the application

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ImageRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateShapes = new System.Windows.Forms.Button();
+            this.btnApplyTransform = new System.Windows.Forms.Button();
+            this.panOriginal = new System.Windows.Forms.Panel();
+            this.panAfterT = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnCreateShapes
+            // 
+            this.btnCreateShapes.Location = new System.Drawing.Point(209, 524);
+            this.btnCreateShapes.Name = "btnCreateShapes";
+            this.btnCreateShapes.Size = new System.Drawing.Size(155, 60);
+            this.btnCreateShapes.TabIndex = 0;
+            this.btnCreateShapes.Text = "Create Shapes";
+            this.btnCreateShapes.UseVisualStyleBackColor = true;
+            this.btnCreateShapes.Click += new System.EventHandler(this.btnCreateShapes_Click);
+            // 
+            // btnApplyTransform
+            // 
+            this.btnApplyTransform.Location = new System.Drawing.Point(401, 524);
+            this.btnApplyTransform.Name = "btnApplyTransform";
+            this.btnApplyTransform.Size = new System.Drawing.Size(155, 59);
+            this.btnApplyTransform.TabIndex = 1;
+            this.btnApplyTransform.Text = "Apply Transformation";
+            this.btnApplyTransform.UseVisualStyleBackColor = true;
+            this.btnApplyTransform.Click += new System.EventHandler(this.btnApplyTransform_Click);
+            // 
+            // panOriginal
+            // 
+            this.panOriginal.Location = new System.Drawing.Point(36, 29);
+            this.panOriginal.Name = "panOriginal";
+            this.panOriginal.Size = new System.Drawing.Size(328, 458);
+            this.panOriginal.TabIndex = 2;
+            // 
+            // panAfterT
+            // 
+            this.panAfterT.Location = new System.Drawing.Point(391, 29);
+            this.panAfterT.Name = "panAfterT";
+            this.panAfterT.Size = new System.Drawing.Size(334, 458);
+            this.panAfterT.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 616);
+            this.Controls.Add(this.panAfterT);
+            this.Controls.Add(this.panOriginal);
+            this.Controls.Add(this.btnApplyTransform);
+            this.Controls.Add(this.btnCreateShapes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateShapes;
+        private System.Windows.Forms.Button btnApplyTransform;
+        private System.Windows.Forms.Panel panOriginal;
+        private System.Windows.Forms.Panel panAfterT;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using Mapack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageRegistration
+{
+    public partial class Form1 : Form
+    {
+        //Shape 1 and Shape 2 are the shapes displayed on the windows form
+        List<Point> Shape1 = new List<Point>();
+        List<Point> Shape2 = new List<Point>();
+        RegistrationTools utils = new RegistrationTools();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// We create two set of points to test our Image Registration
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateShapes_Click(object sender, EventArgs e)
+        {
+            CreatePointLists();
+            //We are going to map the point in the Shape1 to some other points using an arbitrary transformation
+            Transformation T = new Transformation(1.05, 0.05, 15, 22);
+            Shape2 = utils.ApplyTransformation(T, Shape1);
+            Shape2[2] = new Point(Shape2[2].X + 10, Shape2[2].Y + 3); //Creating some noise that does not obey a transformation
+            DrawOnPanel(panOriginal);
+            
+        }
+
+        void DrawOnPanel(Panel pan)
+        {
+            //Drawing the shapes on the panel
+            Pen blue = new Pen(Brushes.Blue, 1);
+            Pen red = new Pen(Brushes.Red, 1);
+            Graphics g = pan.CreateGraphics();
+            DisplayShape(Shape1, blue, g);
+            DisplayShape(Shape2, red, g);
+        }
+
+        void CreatePointLists()
+        {
+            Shape1.Clear();
+            Shape2.Clear();
+            Point p1a = new Point(20, 30);
+            Point p2a = new Point(120, 50);
+            Point p3a = new Point(160, 80);
+            Point p4a = new Point(180, 300);
+            Point p5a = new Point(100, 220);
+            Point p6a = new Point(50, 280);
+            Point p7a = new Point(20, 140);
+            Shape1.Add(p1a); Shape1.Add(p2a);
+            Shape1.Add(p3a); Shape1.Add(p4a);
+            Shape1.Add(p5a); Shape1.Add(p6a);
+            Shape1.Add(p7a);
+        }
+
+        void DisplayShape(List<Point> Shape, Pen pen, Graphics g)
+        {
+            Point? prevPoint = null;
+            foreach(Point p in Shape)
+            {
+                g.DrawEllipse(pen, new Rectangle(p.X - 2, p.Y - 2, 4, 4));
+                if (prevPoint != null) g.DrawLine(pen, (Point)prevPoint, p);
+                prevPoint = p;
+            }
+            g.DrawLine(pen, Shape[0], Shape[Shape.Count - 1]);
+        }
+
+        private void btnApplyTransform_Click(object sender, EventArgs e)
+        {
+            Transformation T = utils.GenerateT(P1:Shape1, P2:Shape2);
+            Shape2 = utils.ApplyTransformation(T, Shape2);
+            DrawOnPanel(panAfterT);
+        }
+    }
+}
